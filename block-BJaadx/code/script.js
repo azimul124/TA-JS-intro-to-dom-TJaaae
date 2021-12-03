a09b5252f382f93d99f692d3c0709c9ff7fc0bc4
Azimul Haque <mailofazim@gmail.com>
2021-12-03 18:55:40 +0530

    final-commit

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -60,3 +60,27 @@ document.querySelector("email");
 // 26. Select the element with type `fieldset` and store in a variable named `fieldsetElm`.
 
 // 27. Select the parent element of the element stored in `fieldsetElm` variable (#5) and log the `typeof` the element.
+
+// 16. Select all the `p` element and store in `allPElement`
+
+// 17. Select all the buttons and log the count of buttons.
+
+// 18. Select all the `label` element and log the count.
+
+// 19. Select all the elements with `id` of `test`
+
+// 20. Select the first element with id `test` using `getElementById`
+
+// 21. Select the parent element of the element stored in `topics` variable (#5) and log the element.
+
+// 22. Select the next element sibling of the element stored in `topics` variable (#5) and log the element.
+
+// 23. Select the previous element sibling of the element stored in `topics` variable (#5) and change the `innerText` property to `Learning About Walking the DOM`.
+
+// 24. Select the first element child of the element stored in `topics` variable (#5) and change the `innerText` property of the element to `This is the first child element`.
+
+// 25. Select the last element child of the element stored in `topics` variable (#5) and log the `typeof` the element.
+
+// 26. Select the element with type `fieldset` and store in a variable named `fieldsetElm`.
+
+// 27. Select the parent element of the element stored in `fieldsetElm` variable (#5) and log the `typeof` the element.
