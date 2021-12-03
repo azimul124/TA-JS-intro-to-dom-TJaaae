1ab4d46ace8ee489d53a2b176d60d32556c4737d
Azimul Haque <mailofazim@gmail.com>
2021-12-03 18:19:49 +0530

    first-commit

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -1,11 +1,15 @@
 // 1. Change the title of the page to `Hello AltCampus!`
-
+document.title = "Hello AltCampus!";
 // 2. Select the element using the children property:
-
+document.body.children[0];
 //    - Select the `h1` element and change the value to `Learning DOM`
-
+document.querySelector("h1");
+h1.innerText = "Learning DOM";
 //    - Select the first `li` element inside the `ul` with class `topics` and change the innerText to `all about document`
+document.querySelector("li");
+li.innerText = "all about document";
 //    - Select the input element with name `email`
+document.querySelector("email");
 
 // 3. Log the number (using console.log) of children of all the `li` element inside the ul with class `topics`
 
